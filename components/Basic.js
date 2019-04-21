d3cc44050d46c4d119a67265447331c06702d438
MAJU STUDENT <majustudent@jinnah.edu>
2019-04-21 18:42:01 +0500

    first commit

---
+++ components/Basic.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {ScrollView } from 'react-native';
+import Header1 from './Header';
+
+class Basic extends Component
+{
+    render(){
+        return(
+            <ScrollView>
+            <Header1/>
+          
+         
+            </ScrollView>
+        );
+        }
+    }
+
+export default Basic;
---
+++ components/Header.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Container, Header, Title, Content, Footer, FooterTab,Accordion, Button, Left, Right, Body, Icon, Text } from 'native-base';
+const dataArray = [
+  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
+export default class AnatomyExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Abdul Moiz</Title>
+          </Body>
+          <Right />
+        </Header>
+       <Content padder style={{ backgroundColor: "white" }}>
+          <Accordion
+            dataArray={dataArray}
+            animation={true}
+            expanded={true}
+            renderHeader={this._renderHeader}
+            renderContent={this._renderContent}
+          />
+          </Content>
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Icon name="apps" />
+            </Button>
+            <Button>
+              <Icon name="camera" />
+            </Button>
+            <Button active>
+              <Icon active name="navigate" />
+            </Button>
+            <Button>
+              <Icon name="person" />
+            </Button>
+          </FooterTab>
+        </Footer>
+       </Container> 
+    );
+  }
+}
